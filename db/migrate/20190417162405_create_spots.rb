dfc39a6bad2d0f3427075cfcb2e7f732cfad4d22
Dave Watson <watson_da@hotmail.co.uk>
2019-04-23 14:42:56 +0800

    added type to Spots table

--- db/migrate/20190417162405_create_spots.rb
+++ db/migrate/20190417162405_create_spots.rb
@@ -3,6 +3,7 @@ class CreateSpots < ActiveRecord::Migration[5.2]
     create_table :spots do |t|
       t.integer :spot_rating
       t.string :difficulty_rating
+      t.string :type
       t.string :default_image
       t.references :user, foreign_key: true
       t.timestamps null: false
