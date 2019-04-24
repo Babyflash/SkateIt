2b5ab4aa05c550ee42d55e501f778241e6db8480
Dave Watson <watson_da@hotmail.co.uk>
2019-04-24 14:39:10 +0800

    updated type column to spot_type in spots and changed logitude & latitude to float in locations

--- db/migrate/20190417162405_create_spots.rb
+++ db/migrate/20190417162405_create_spots.rb
@@ -3,7 +3,7 @@ class CreateSpots < ActiveRecord::Migration[5.2]
     create_table :spots do |t|
       t.integer :spot_rating
       t.integer :difficulty_rating
-      t.string :type
+      t.string :spot_type
       t.string :default_image
       t.references :user, foreign_key: true
       t.timestamps null: false
