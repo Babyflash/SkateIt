a89da3fa57570a5fd0e0b5c77d66c75dbe49f10b
Dave Watson <watson_da@hotmail.co.uk>
2019-04-24 14:42:25 +0800

    changed logitude & latitude to float in all tables

--- db/migrate/20190417162851_create_spot_content.rb
+++ db/migrate/20190417162851_create_spot_content.rb
@@ -2,8 +2,8 @@ class CreateSpotContent < ActiveRecord::Migration[5.2]
   def change
     create_table :spot_contents do |t|
       t.string :media_url
-      t.string :media_geotag_longitude
-      t.string :media_geotag_latitude
+      t.float :geo_long
+      t.float :geo_lat
       t.references :spot, foreign_key: true
       t.timestamps null: false
     end
--- db/migrate/20190417163201_create_post_content.rb
+++ db/migrate/20190417163201_create_post_content.rb
@@ -2,8 +2,8 @@ class CreatePostContent < ActiveRecord::Migration[5.2]
   def change
     create_table :post_contents do |t|
       t.string :media_url
-      t.string :media_geotag_longitude
-      t.string :media_geotag_latitude
+      t.float :geo_long
+      t.float :geo_lat
       t.references :post, foreign_key: true
       t.timestamps null: false
     end
