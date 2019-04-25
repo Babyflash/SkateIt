5bb4653905f343ac152f365eead15fa444cc556c
Dave Watson <watson_da@hotmail.co.uk>
2019-04-25 16:37:41 +0800

    added migration add address column to spots table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_083512) do
+ActiveRecord::Schema.define(version: 2019_04_25_082630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2019_04_24_083512) do
     t.datetime "updated_at", null: false
     t.float "geo_lat"
     t.float "geo_lng"
+    t.string "address"
     t.index ["user_id"], name: "index_spots_on_user_id"
   end
 
