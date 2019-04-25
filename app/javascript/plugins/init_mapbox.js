bc86ab14c39c089b565554233f634947e98719ec
Chinzorig <pchinzoo@gmail.com>
2019-04-25 18:25:21 +0800

    done map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -7,78 +7,142 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { duration: 2000, padding: 70, maxZoom: 15 });
 };
 
+function setMarkerLngLat(marker) {
+  var lngLat = marker.getLngLat();
+  const coordinates = document.getElementById('coordinates');
+  if(coordinates){
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+    const lat = document.getElementById('spot_geo_lat');
+    const lng = document.getElementById('spot_geo_lng');
+    lat.value = lngLat.lat;
+    lng.value = lngLat.lng;
+  }
+}
+
+function loadPostsMarker(map, mapElement) {
+  const markers = JSON.parse(mapElement.dataset.markers);
+  if(markers) {
+    markers.forEach((marker) => {
+
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '56px';
+      element.style.height = '56px';
+
+      new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(new mapboxgl.Popup({ offset: 25 })
+        .setHTML(marker.infoWindow.content))
+        .addTo(map);
+    });
+
+    fitMapToMarkers(map, markers)
+  }
+}
+
+function loadDraggableMarker(map) {
+  var marker = new mapboxgl.Marker({
+    draggable: true
+  })
+  .setLngLat([0, 0])
+  .addTo(map);
+
+  marker.on('dragend', function() {
+    setMarkerLngLat(marker);
+  });
+
+  var geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+  })
+
+  map.addControl(geocoder);
+
+  geocoder.on('results', function(results) {
+    const coor = results.features[0].center
+
+    marker.setLngLat(coor).addTo(map)
+  })
+
+  map.on('click', function(e) {
+    marker.setLngLat(e.lngLat).addTo(map)
+    setMarkerLngLat(marker);
+  });
+}
+
+function loadSinglePostMap(map, mapElement) {
+  const spot = JSON.parse(mapElement.dataset.spot);
+  console.log(`${spot.geo_lat}:${spot.geo_lng}`)
+  if(spot){
+    // map.flyTo({
+    //   zoom: 8,
+    //   center: [spot.geo_lat, spot.geo_lng]
+    // });
+
+    const bounds = new mapboxgl.LngLatBounds();
+    bounds.extend([ spot.geo_lng, spot.geo_lat ])
+    map.fitBounds(bounds, { duration: 2000, padding: 70, maxZoom: 15 });
+
+    var marker = new mapboxgl.Marker({
+      draggable: false
+    })
+    .setLngLat([spot.geo_lat, spot.geo_lng])
+    .addTo(map);
+  }
+
+  loadPostsMarker(map, mapElement);
+}
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
+  if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
 
-    const markers = JSON.parse(mapElement.dataset.markers);
-    if(markers) {
-      markers.forEach((marker) => {
 
-        const element = document.createElement('div');
-        element.className = 'marker';
-        element.style.backgroundImage = `url('${marker.image_url}')`;
-        element.style.backgroundSize = 'contain';
-        element.style.width = '56px';
-        element.style.height = '56px';
+    const viewType = mapElement.dataset.viewType;
 
-        new mapboxgl.Marker(element)
-          .setLngLat([ marker.lng, marker.lat ])
-          .setPopup(new mapboxgl.Popup({ offset: 25 })
-          .setHTML(marker.infoWindow.content))
-          .addTo(map);
-      });
-
-      fitMapToMarkers(map, markers)
+    switch(viewType){
+      case 'spots': loadPostsMarker(map, mapElement); break;
+      case 'create_spot': loadDraggableMarker(map); break;
+      case 'spot': loadSinglePostMap(map, mapElement); break;
     }
 
-    var marker = new mapboxgl.Marker({
-      draggable: true
-    })
-    .setLngLat([0, 0])
-    .addTo(map);
+    // var marker = new mapboxgl.Marker({
+    //     draggable: true
+    //   })
+    //   .setLngLat([0, 0])
+    //   .addTo(map);
 
-    var geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-    })
+    // var geocoder = new MapboxGeocoder({
+    //   accessToken: mapboxgl.accessToken,
+    // })
 
-    geocoder.on('results', function(results) {
-      console.log(results);
-      const coor = results.features[0].center
-      const lat = coor[0]
-      const long = coor[1]
+    // geocoder.on('results', function(results) {
+    //   console.log(results);
+    //   const coor = results.features[0].center
+    //   const lat = coor[0]
+    //   const long = coor[1]
 
-      marker.setLngLat(coor).addTo(map)
-    })
+    //   marker.setLngLat(coor).addTo(map)
+    // })
 
-    map.addControl(geocoder);
+    // map.addControl(geocoder);
 
-    map.on('click', function(e) {
-      marker.setLngLat(e.lngLat).addTo(map)
-      onDragEnd();
-    });
+    // map.on('click', function(e) {
+    //   marker.setLngLat(e.lngLat).addTo(map)
+    //   setMarkerLngLat(marker);
+    // });
 
-    function onDragEnd() {
-      var lngLat = marker.getLngLat();
-      const coordinates = document.getElementById('coordinates');
-      coordinates.style.display = 'block';
-      coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
-      const lat = document.getElementById('spot_geo_lat');
-      const lng = document.getElementById('spot_geo_lng');
-      lat.value = lngLat.lat;
-      lng.value = lngLat.lng;
-    }
 
-    marker.on('dragend', onDragEnd);
+    // marker.on('dragend', setMarkerLngLat(marker));
   }
 };
-
-
 
 export { initMapbox };
