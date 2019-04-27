da949d4b09119ae28d338c542853b03e00d6f4f1
Sevan Golnazarian <sgolnaza@gmail.com>
2019-04-27 15:38:01 +0800

    token authentication added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  # Token Authentication
+  acts_as_token_authenticatable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
   has_many :favorites
   has_many :posts
   has_many :checkins
