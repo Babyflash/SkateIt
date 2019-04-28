0c0301f3f62cc2babbe3900bfeb876162684c478
Sevan Golnazarian <sgolnaza@gmail.com>
2019-04-28 23:48:59 +0800

    edited json to spots

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -1,14 +1,22 @@
 class Api::V1::SpotsController < Api::V1::BaseController
+  # acts_as_token_authentication_handler_for Spot, except: [ :index ]
+
   respond_to :json
 
   def index
     @spots = Spot.all
-
+    # .sort((a, b) => (a.spot_type > b.spot_type) ? 1 : ((b.spot_type > a.spot_type) ? -1 : 0))
     p "testing------------------"
     p params[:spot][:name]
     p "testing------------------"
+    
+    @allSpots = {}
 
-    render json: @spots
+    Spot.distinct.pluck(:spot_type).each do |type|
+      @allSpots[type] = Spot.where(:spot_type => type ).order(created_at: :desc)
+    end
+  
+    render json: @allSpots
   end
 
   def set_params
