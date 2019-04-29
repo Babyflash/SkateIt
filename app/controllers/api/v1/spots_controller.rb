20a340c9599854a973797383f6bc0c0c1d563813
Sevan Golnazarian <sgolnaza@gmail.com>
2019-04-29 13:43:26 +0800

    added test output that displays the spots response and user

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -6,16 +6,16 @@ class Api::V1::SpotsController < Api::V1::BaseController
   def index
     @spots = Spot.all
     # .sort((a, b) => (a.spot_type > b.spot_type) ? 1 : ((b.spot_type > a.spot_type) ? -1 : 0))
-    p "testing------------------"
-    p params[:spot][:name]
-    p "testing------------------"
+   
     
     @allSpots = {}
 
     Spot.distinct.pluck(:spot_type).each do |type|
       @allSpots[type] = Spot.where(:spot_type => type ).order(created_at: :desc)
     end
-  
+     p "testing------------------"
+    p @allSpots
+    p "testing------------------"
     render json: @allSpots
   end
 
